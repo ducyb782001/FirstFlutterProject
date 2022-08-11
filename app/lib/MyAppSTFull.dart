bba8417080338d591724894e7eb313ad2b3de2e0
ducndthe153148 <ducndthe153148@fpt.edu.vn>
2022-08-11 14:43:54 +0700

    intl package format edate

--- app/lib/MyAppSTFull.dart
+++ app/lib/MyAppSTFull.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 // StatefulWidget: Has internal "state"
 // Khi state thay đổi thì hàm build reload => giao diện reload
@@ -17,12 +18,42 @@ class MyAppStatefull extends StatefulWidget {
   }
 }
 
-class _MyAppState extends State<MyAppStatefull> {
+class _MyAppState extends State<MyAppStatefull> with WidgetsBindingObserver {
+  // When a widget is created and running, there are 3 functions that we may concern
+  // 1. init state
+  // 2. build() is trigger when we use setState()
+  // 3. dispose() is called when state/ widget object is removed
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    print('Run Init state');
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    print('Run dispose ');
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    // TODO: implement didChangeAppLifecycleState
+    super.didChangeAppLifecycleState(state);
+    if (state == AppLifecycleState.paused) {
+      print('App is in background mode');
+    } else if (state == AppLifecycleState.resumed) {
+      print('App is in foreground mode');
+    }
+  }
+
   String _email = ''; // This is state
   final emailEditingController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
+    print('Run build()');
+    DateTime now = new DateTime.now();
     return MaterialApp(
       title: 'This is stateful widget',
       home: Scaffold(
@@ -61,7 +92,7 @@ class _MyAppState extends State<MyAppStatefull> {
                 style: TextStyle(fontSize: 30, color: Colors.blueAccent),
               ),
               Text(
-                'Line 3: Name: ${widget.name} has age ${widget.age}',
+                DateFormat('yyyy/MM/dd').format(now),
                 style: TextStyle(fontSize: 30, color: Colors.green.shade900),
               )
             ],
